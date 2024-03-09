08e39ffc1bdd90c0849f30ac56f9550950c49e09
D-kinshy <d.kinshy@gmail.com>
2024-03-09 21:43:13 +0300

    after add branch2 try oush to main branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
       <>
             <h1>branch 2</h1>
-            <h2>after add branch 2</h2>
+            <h2>after add branch 2 but try to push to main branch</h2>
             <h1>faesal zaeed</h1>
             <h1>MR.Code</h1>
         
