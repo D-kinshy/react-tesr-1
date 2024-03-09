1da9d66ce66ccb714c301442824683f2523abeeb
D-kinshy <d.kinshy@gmail.com>
2024-03-09 21:38:15 +0300

    after make branch2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
       <>
             <h1>branch 2</h1>
+            <h2>after add branch 2</h2>
             <h1>faesal zaeed</h1>
             <h1>MR.Code</h1>
         
